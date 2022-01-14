d5dff48c15401a6df7fe970c5f0609b0abe520c6
Sven <sven.vander.mierde@gmail.com>
2022-01-14 16:22:47 +0100

    building and importing first component

---
+++ Section 07 - Vue CLI/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <ul>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 65432 10",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
