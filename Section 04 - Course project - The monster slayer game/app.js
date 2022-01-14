553053c804fc27e74cdaae1d2b7f0fb203a30407
Sven <sven.vander.mierde@gmail.com>
2022-01-14 12:28:18 +0100

    cleaning code

--- Section 04 - Course project - The monster slayer game/app.js
+++ Section 04 - Course project - The monster slayer game/app.js
@@ -4,7 +4,6 @@ const app = Vue.createApp({
       playerHealth: 100,
       monsterHealth: 100,
       rounds: 0,
-      hasHealed: false,
       moves: [],
       winner: null,
     };
@@ -48,49 +47,47 @@ const app = Vue.createApp({
       this.hasHealed = false;
       const dmg = getRnd(12, 5);
       this.monsterHealth -= dmg;
-      this.moves.push(setMove('Player', 'Attack', dmg));
+      this.addMove('Player', 'Attack', dmg);
       this.attackPlayer();
     },
     attackPlayer() {
       const dmg = getRnd(8, 18);
       this.playerHealth -= dmg;
-      this.moves.push(setMove('Monster', 'Attack', dmg));
+      this.addMove('Monster', 'Attack', dmg);
     },
     specialAttackMonster() {
       this.rounds++;
       this.hasHealed = false;
       const dmg = getRnd(10, 25);
       this.monsterHealth -= dmg;
-      this.moves.push(setMove('Player', 'Special Attack', dmg));
+      this.addMove('Player', 'Attack', dmg);
       this.attackPlayer();
     },
     healPlayer() {
       this.rounds++;
-      if (this.hasHealed) {
-        this.moves.push(setMove('Player', 'Heal', 'fail'));
+      const hp = getRnd(7, 3);
+      if (this.playerHealth + hp > 100) {
+        this.playerHealth = 100;
       } else {
-        this.hasHealed = true;
-        const hp = getRnd(5, 1);
-        if (this.playerHealth + hp > 100) {
-          this.playerHealth = 100;
-        } else {
-          this.playerHealth += hp;
-        }
-
-        this.moves.push(setMove('Player', 'Heal', hp));
+        this.playerHealth += hp;
       }
+
+      this.addMove('Player', 'Heal', hp);
+
       this.attackPlayer();
     },
     newGame() {
       this.playerHealth = 100;
       this.monsterHealth = 100;
       this.rounds = 0;
-      this.hasHealed = false;
       this.moves = [];
       this.winner = null;
     },
     surrender() {
       this.winner = 'monster';
+    },
+    addMove(who, what, value) {
+      this.moves.unshift({ name: who, move: what, value: value });
     },
   },
 });
@@ -99,7 +96,4 @@ app.mount('#game');
 
 function getRnd(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
-function setMove(name, move, value) {
-  return { name: name, move: move, value: value };
 }
