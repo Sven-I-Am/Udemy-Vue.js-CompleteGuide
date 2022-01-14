13458e566a823f1f2e069f9d5abfb661e11f9745
Sven <sven.vander.mierde@gmail.com>
2022-01-14 14:48:51 +0100

    first simplecomponent

---
+++ Section 06 - Introducing Components/app.js
@@ -0,0 +1,45 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [
+        {
+          id: 'julie',
+          name: 'Julie Jones',
+          phone: '01234 5678 991',
+          email: 'manuel@localhost.com',
+        },
+      ],
+    };
+  },
+});
+
+app.component('friend-contact', {
+  template: `
+    <li>
+      <h2>{{friend.name}}</h2>
+      <button @click="toggleDetails">{{ isVisible ? 'Hide' : 'Show' }} Details</button>
+      <ul v-if="isVisible">
+          <li><strong>Phone:</strong> {{friend.phone}}</li>
+          <li><strong>Email:</strong> {{friend.email}}</li>
+      </ul>
+    </li>    
+    `,
+  data() {
+    return {
+      isVisible: false,
+      friend: {
+        id: 'manuel',
+        name: 'Manuel Lorenz',
+        phone: '01234 5678 991',
+        email: 'manuel@localhost.com',
+      },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.isVisible = !this.isVisible;
+    },
+  },
+});
+
+app.mount('#app');
