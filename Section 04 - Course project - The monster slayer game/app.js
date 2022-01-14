36098b37fcd8267c272f886b6e1d14836af5f91d
Sven <sven.vander.mierde@gmail.com>
2022-01-14 12:14:28 +0100

    all buttons work

--- Section 04 - Course project - The monster slayer game/app.js
+++ Section 04 - Course project - The monster slayer game/app.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
       rounds: 0,
       hasHealed: false,
       moves: [],
+      winner: null,
     };
   },
   computed: {
@@ -19,41 +20,81 @@ const app = Vue.createApp({
       return this.rounds % 3 !== 0;
     },
   },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.playerHealth = 0;
+        this.monsterHealth = 0;
+        this.winner = 'draw';
+      } else if (value <= 0) {
+        this.playerHealth = 0;
+        this.winner = 'monster';
+      }
+    },
+    monsterHealth(value) {
+      if (value < +0 && this.playerHealth < +0) {
+        this.playerHealth = 0;
+        this.monsterHealth = 0;
+        this.winner = 'draw';
+      } else if (value < +0) {
+        this.monsterHealth = 0;
+        this.winner = 'player';
+      }
+    },
+  },
   methods: {
     attackMonster() {
       this.rounds++;
-      const dmg = calcDmg(12, 5);
+      this.hasHealed = false;
+      const dmg = getRnd(12, 5);
       this.monsterHealth -= dmg;
       this.moves.push(setMove('Player', 'Attack', dmg));
       this.attackPlayer();
     },
     attackPlayer() {
-      const dmg = calcDmg(8, 18);
+      const dmg = getRnd(8, 18);
       this.playerHealth -= dmg;
       this.moves.push(setMove('Monster', 'Attack', dmg));
     },
     specialAttackMonster() {
       this.rounds++;
-      const dmg = calcDmg(10, 25);
+      this.hasHealed = false;
+      const dmg = getRnd(10, 25);
       this.monsterHealth -= dmg;
       this.moves.push(setMove('Player', 'Special Attack', dmg));
       this.attackPlayer();
     },
     healPlayer() {
-      if (hasHealed) {
-        move = { name: 'Player', move: 'Heal fails' };
+      this.rounds++;
+      if (this.hasHealed) {
+        this.moves.push(setMove('Player', 'Heal', 'fail'));
       } else {
-        const hp = calcDmg(5, 1);
-        this.playerHealth += hp;
-        move = { name: 'Player', move: 'Heals for ' + hp + ' points' };
+        this.hasHealed = true;
+        const hp = getRnd(5, 1);
+        if (this.playerHealth + hp > 100) {
+          this.playerHealth = 100;
+        } else {
+          this.playerHealth += hp;
+        }
+
+        this.moves.push(setMove('Player', 'Heal', hp));
       }
+      this.attackPlayer();
+    },
+    newGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.rounds = 0;
+      this.hasHealed = false;
+      this.moves = [];
+      this.winner = null;
     },
   },
 });
 
 app.mount('#game');
 
-function calcDmg(min, max) {
+function getRnd(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 function setMove(name, move, value) {
