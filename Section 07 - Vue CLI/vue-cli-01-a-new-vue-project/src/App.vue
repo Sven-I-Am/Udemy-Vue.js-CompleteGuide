1af75455ca80a4ae4fb7590565c6f992a9e4d72f
Sven <sven.vander.mierde@gmail.com>
2022-01-14 16:47:22 +0100

    passing props to our component

--- Section 07 - Vue CLI/vue-cli-01-a-new-vue-project/src/App.vue
+++ Section 07 - Vue CLI/vue-cli-01-a-new-vue-project/src/App.vue
@@ -4,7 +4,16 @@
       <h1>My friends</h1>
     </header>
     <ul>
-      <friend-contact></friend-contact>
+      <friend-contact
+        name="Manuel Lorenzs"
+        phoneNumber="0123 45678 90"
+        emailAddress="manuel@localhost.com"
+      ></friend-contact>
+      <friend-contact
+        name="Julie Jones"
+        phoneNumber="0987 65432 10"
+        emailAddress="julie@localhost.com"
+      ></friend-contact>
     </ul>
   </section>
 </template>
