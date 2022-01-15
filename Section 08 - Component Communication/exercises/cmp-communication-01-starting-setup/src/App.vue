b386b0e9a1136305c692497a0fe8892ef5237b3a
Sven <sven.vander.mierde@gmail.com>
2022-01-15 07:54:00 +0100

    prep for dynamic values

---
+++ Section 08 - Component Communication/exercises/cmp-communication-01-starting-setup/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        id="1"
+        name="Manuel Lorenzs"
+        phoneNumber="0123 45678 90"
+        emailAddress="manuel@localhost.com"
+        setFavorite="0"
+      ></friend-contact>
+      <friend-contact
+        id="2"
+        name="Julie Jones"
+        phoneNumber="0987 65432 10"
+        emailAddress="julie@localhost.com"
+        setFavorite="1"
+      ></friend-contact>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "1",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "2",
+          name: "Julie Jones",
+          phone: "0987 65432 10",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ Section 08 - Component Communication/exercises/cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,126 @@
+<template>
+  <li :key="id">
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ isVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">
+      {{ isFavorite ? "Unset" : "Set" }} Favorite
+    </button>
+    <ul v-if="isVisible">
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>Email:</strong> {{ emailAddress }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  //   props: ["id", "name", "phoneNumber", "emailAddress"],
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    setFavorite: {
+      type: String,
+      required: false,
+      default: "0",
+      validator: function (value) {
+        return value === "1" || value === "0";
+      },
+    },
+  },
+  data() {
+    return {
+      isVisible: false,
+      isFavorite: this.setFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.isVisible = !this.isVisible;
+    },
+    toggleFavorite() {
+      this.isFavorite = !this.isFavorite;
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
