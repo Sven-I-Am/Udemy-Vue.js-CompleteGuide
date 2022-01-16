cffec07e7b893dfca9ae8adab701d54e55fa3dee
Sven <sven.vander.mierde@gmail.com>
2022-01-16 10:29:49 +0100

    fixed tab selection

--- Section 10 - Course project - Learning resource app/src/App.vue
+++ Section 10 - Course project - Learning resource app/src/App.vue
@@ -1,43 +1,20 @@
 <template>
   <the-header :title="title"></the-header>
   <the-resources></the-resources>
-  <stored-resources
-    :resources="storedResources"
-    v-if="selectedComponent === 'stored-resources'"
-  ></stored-resources>
-  <add-resources v-if="selectedComponent === 'add-resources'"></add-resources>
 </template>
 
 <script>
-import StoredResources from './components/learningResources/StoredResources.vue';
-import AddResources from './components/learningResources/AddResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue';
 import TheResources from './components/learningResources/TheResources.vue';
 
 export default {
   components: {
-    StoredResources,
-    AddResources,
     TheHeader,
     TheResources,
   },
   data() {
     return {
       title: 'RememberMe',
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation.',
-          link: 'https://v3.vuejs.org/',
-        },
-        {
-          id: 'google',
-          title: 'Google',
-          description: 'Learn to Google...',
-          link: 'https://www.google.com',
-        },
-      ],
     };
   },
 };
--- Section 10 - Course project - Learning resource app/src/components/learningResources/TheResources.vue
+++ Section 10 - Course project - Learning resource app/src/components/learningResources/TheResources.vue
@@ -1,19 +1,42 @@
 <template>
   <base-card>
-    <base-button @click.prevent="setSelectedComponent('stored-resources')"
+    <base-button @click="setSelectedComponent('stored-resources')"
       >Stored Resources</base-button
     >
-    <base-button @click.prevent="setSelectedComponent('add-resources')"
+    <base-button @click="setSelectedComponent('add-resources')"
       >Add Resource</base-button
     >
   </base-card>
+  <component :is="selectedTab"></component>
 </template>
 
 <script>
+import StoredResources from './StoredResources.vue';
+import AddResources from './AddResources.vue';
 export default {
+  components: { StoredResources, AddResources },
   data() {
     return {
       selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://v3.vuejs.org/',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google...',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
     };
   },
 
